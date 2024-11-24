b2af7549bb466547f1dd0a07c88a2b0a9e6f907e
Brandon Wong <bwong109@usc.edu>
2024-11-23 20:31:29 -0800

    Delete scripts directory

--- scripts/parallax.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', function () {
-    const parallax = document.querySelector('.parallax-section');
-    let scrollPosition = window.pageYOffset;
-    parallax.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
-  });
-  
